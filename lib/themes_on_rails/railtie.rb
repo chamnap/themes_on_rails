2936fa4157bac87fdb2e3df7a3a0fac699d965f3
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-05-14 15:44:14 +0700

    precompile themes for all environments due to sprockets-rails#84, rails 4.1

--- lib/themes_on_rails/railtie.rb
+++ lib/themes_on_rails/railtie.rb
@@ -13,12 +13,10 @@ module ThemesOnRails
       end
     end
 
-    if !Rails.env.development? && !Rails.env.test?
-      initializer "themes_on_rails.precompile" do |app|
-        app.config.assets.precompile += [ Proc.new { |path, fn| fn =~ /app\/themes/ && !%w(.js .css).include?(File.extname(path)) } ]
-        app.config.assets.precompile += Dir["app/themes/*"].map { |path| "#{path.split('/').last}/all.js" }
-        app.config.assets.precompile += Dir["app/themes/*"].map { |path| "#{path.split('/').last}/all.css" }
-      end
+    initializer "themes_on_rails.precompile" do |app|
+      app.config.assets.precompile += [ Proc.new { |path, fn| fn =~ /app\/themes/ && !%w(.js .css).include?(File.extname(path)) } ]
+      app.config.assets.precompile += Dir["app/themes/*"].map { |path| "#{path.split('/').last}/all.js" }
+      app.config.assets.precompile += Dir["app/themes/*"].map { |path| "#{path.split('/').last}/all.css" }
     end
 
   end
