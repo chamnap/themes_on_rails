094efedc9c6a2af69cf16b39160f86536f2359cc
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-11-14 14:34:33 +0700

    fix nested inside asset precompile

--- lib/themes_on_rails/engine.rb
+++ lib/themes_on_rails/engine.rb
@@ -25,7 +25,10 @@ module ThemesOnRails
           basename = path.split('/').last
           if !%w(.js .css).include?(File.extname(path))
             true
-          elsif basename.in?(['all.js', 'all.css']) || basename.start_with?('all_')
+          elsif path =~ /^[^\/]+\/all(_.+)?\.(js|css)$/
+            # 1. don't allow nested: theme_a/responsive/all.js
+            # 2. allow start_with all_
+            # 3. alllow all.js and all.css
             true
           else
             false
