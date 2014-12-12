60f5af70fa1aedd4177ca83d32d85c8fd9668c45
Chamnap Chhorn <chamnapchhorn@gmail.com>
2014-12-12 12:00:48 +0700

    fix #all

--- lib/themes_on_rails.rb
+++ lib/themes_on_rails.rb
@@ -7,6 +7,6 @@ module ThemesOnRails
   autoload :ControllerAdditions, 'themes_on_rails/controller_additions'
 
   def self.all
-    Dir.entries('app/themes').sort[2..-1]
+    Dir.entries('app/themes/*').select { |fn| !fn.start_with?('.') && File.directory?(fn) }.sort[2..-1]
   end
 end
