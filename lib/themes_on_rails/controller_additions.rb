1a3fc3b7ed0309bee88d2dad80e3112356649d48
Phia Vang <pnvang@gmail.com>
2015-04-13 16:07:14 -0500

    nil check.

--- lib/themes_on_rails/controller_additions.rb
+++ lib/themes_on_rails/controller_additions.rb
@@ -4,6 +4,7 @@ module ThemesOnRails
 
     module ClassMethods
       def theme(theme, options={})
+        return unless theme.present?
         @_theme         = theme
         @_theme_options = options
         ThemesOnRails::ActionController.apply_theme(self, theme, options)
