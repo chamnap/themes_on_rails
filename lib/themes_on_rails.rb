454a06f4a285d5307a251b01cf6991379e10e4e5
Joel Low <joel@joelsplace.sg>
2016-03-12 12:46:08 +0800

    Implements theme caching in production.
    Cached templates are kept together with the template resolver in ActionView. Creating a new template resolver at every request would therefore result in templates not being cached.

--- lib/themes_on_rails.rb
+++ lib/themes_on_rails.rb
@@ -3,8 +3,9 @@ require 'themes_on_rails/engine'
 require 'active_support/concern'
 
 module ThemesOnRails
-  autoload :ActionController,    'themes_on_rails/action_controller'
-  autoload :ControllerAdditions, 'themes_on_rails/controller_additions'
+  autoload :ActionController,        'themes_on_rails/action_controller'
+  autoload :ControllerAdditions,     'themes_on_rails/controller_additions'
+  autoload :FileSystemResolverCache, 'themes_on_rails/file_system_resolver_cache'
 
   def self.all
     Dir.glob("app/themes/*").select { |fn| !fn.start_with?('.') && File.directory?(fn) }.map { |fn| fn.split('/').last }
