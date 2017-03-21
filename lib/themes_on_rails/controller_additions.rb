263ce95eaa7ffff369d0d2d350f4b1f1d8895d15
Romeu Fonseca <rfonseca@skintertainment.com>
2017-03-21 09:43:35 -0300

    usign rails 5 way of concern

--- lib/themes_on_rails/controller_additions.rb
+++ lib/themes_on_rails/controller_additions.rb
@@ -2,7 +2,7 @@ module ThemesOnRails
   module ControllerAdditions
     extend ActiveSupport::Concern
 
-    module ClassMethods
+    class_methods do
       def theme(theme, options={})
         @_theme         = theme
         @_theme_options = options
